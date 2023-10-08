eef5aebe476d46ec6cc4f18839272f5eb6cd2cb7
Shyam <m.shyamsundar37@gmail.com>
2023-10-09 01:32:16 +0530

    changed data source to api

--- src/components/FoodOrder/FoodOrderRoot.tsx
+++ src/components/FoodOrder/FoodOrderRoot.tsx
@@ -1,7 +1,24 @@
+import { useContext, useEffect } from "react";
 import Content from "./Content";
 import Header from "./Header";
+import axios from "axios";
+import { FoodContext, FoodItem } from "../../context/food-context";
 
 export default function FoodOrderRoot() {
+  const foodCtx = useContext(FoodContext);
+  useEffect(() => {
+    async function get() {
+      var foodData = (
+        await axios.get<Array<FoodItem>>("http://rproj.somee.com/foods")
+      ).data; //http://rproj.somee.com/foods
+      console.log(foodData);
+      foodData = foodData.map((item) => {
+        return { ...item, chosenCount: 0 };
+      });
+      foodCtx.setAllFoodItems(foodData);
+    }
+    get();
+  }, []);
   return (
     <>
       <Header />
--- src/context/food-context.tsx
+++ src/context/food-context.tsx
@@ -11,56 +11,20 @@ export type FoodItem = {
 type ContextType = {
   foodItems: Array<FoodItem>;
   incDecFoodItem: (id: number, isAdd: boolean) => void;
+  setAllFoodItems: (items: Array<FoodItem>) => void;
 };
-const food: Array<FoodItem> = [
-  {
-    id: 0,
-    name: "Pizza",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTokGJo9_cNfnDsZhVtmx-_aoX6gcWwCaGkgKn_CRLOapXgsRU3HyU2ogNEjfMwpScyd-I&usqp=CAU",
-    chosenCount: 0,
-    price: 120,
-  },
-  {
-    id: 1,
-    name: "Burger",
-    image:
-      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
-    chosenCount: 0,
-    price: 100,
-  },
-  {
-    id: 2,
-    name: "Tandoori Chicken",
-    image:
-      "https://www.cubesnjuliennes.com/wp-content/uploads/2022/12/Tandoori-Chicken-Recipe.jpg",
-    chosenCount: 0,
-    price: 210,
-  },
-  {
-    id: 3,
-    name: "Fish Curry",
-    image:
-      "https://static.toiimg.com/thumb/92487537.cms?imgsize=121772&width=800&height=800",
-    chosenCount: 0,
-    price: 190,
-  },
-  {
-    id: 4,
-    name: "Mutton Mandi",
-    image:
-      "https://b.zmtcdn.com/data/dish_photos/263/6bb94bcb64ebbba8ca961d42fdbb0263.jpg",
-    chosenCount: 0,
-    price: 399,
-  },
-];
+
 export const FoodContext = createContext<ContextType>({
   foodItems: [],
   incDecFoodItem: (id: number, isAdd: boolean) => {},
+  setAllFoodItems: (items: Array<FoodItem>) => {},
 });
 
 export default function FoodContextProvider(props: PropsWithChildren) {
-  const [foodItems, setFoodItems] = useState<Array<FoodItem>>(food);
+  const [foodItems, setFoodItems] = useState<Array<FoodItem>>([]);
+  function setAllFoodItems(items: Array<FoodItem>) {
+    setFoodItems((prev) => [...items]);
+  }
   function incDecFoodItem(id: number, isAdd: boolean) {
     const index = foodItems.findIndex((f) => f.id == id);
     const item = foodItems[index];
@@ -72,7 +36,11 @@ export default function FoodContextProvider(props: PropsWithChildren) {
   }
   return (
     <FoodContext.Provider
-      value={{ foodItems: foodItems, incDecFoodItem: incDecFoodItem }}
+      value={{
+        foodItems: foodItems,
+        incDecFoodItem: incDecFoodItem,
+        setAllFoodItems: setAllFoodItems,
+      }}
     >
       {props.children}
     </FoodContext.Provider>
